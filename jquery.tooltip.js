52c82992d9629b92778385d554b357e8cb2a3b8f
Максим <33806971+HomsVO@users.noreply.github.com>
2018-06-30 16:26:01 +0300

    Ну вроде бы готово :D

--- jquery.tooltip.js
+++ jquery.tooltip.js
@@ -1,32 +1,44 @@
 (function($){
+	//Объект с передаваемыми параметрами
 	var parameters = {
 		position:'top',
 		text:'simple text'
 	};
-
-	var create = function () {
+	//Создания тултипа
+	var createTooltipBlock = function () {
 		return $('<div></div>').addClass('tooltip').text(parameters.text);
 	}
-	var setPos = function(self,tooltip){
+	//Функция задания позиции тултипу
+	var setPosForTooltipBlock = function(self,tooltip){
 		$('body').append(tooltip);
 		var left;
 		var top;
 		switch(parameters.position) {
 			case 'top': 
-				left = self.offset().left - (tooltip.width()/2) + (self.width()/2);
+				left = self.offset().left - (tooltip.width()/2) + (self.width()/2) - 20;
 				top = self.offset().top - tooltip.height() - 40;
 			break;
 			case 'bottom': 
-				left = self.offset().left + (self.width()/2) - 20;
+				left = self.offset().left - (self.width()/2) - 20;
 				top = self.offset().top + self.height() + 20;
 			break;
+			case 'left': 
+				left = self.offset().left - tooltip.width() - self.width()/2 - 10;
+				top = self.offset().top + self.height()/2 - tooltip.height() + 25;
+			break;
+			case 'right': 
+				left = self.offset().left + self.width() + 20;
+				top = self.offset().top + self.height()/2 - tooltip.height() + 25;
+			break;
 		}
-		return tooltip.offset({'left':left,'top':top});
-			}
-	var show = function(){
-		showingTooltip = setPos($(this),create());
+		return tooltip.offset({'left':left,'top':top})
+		}
+	//Показать тултип
+	var showTooltipBlock = function(){
+		showingTooltip = setPosForTooltipBlock($(this),createTooltipBlock());
 	}
-	var hide = function(){
+	//Скрыть тултип
+	var hideTooltipBlock = function(){
 		if(showingTooltip){
 			showingTooltip.remove();
 		}
@@ -34,7 +46,7 @@
   $.fn.tooltip = function(settings){
   		parameters = $.extend(parameters,settings)
   		var showingTooltip;
-  		this.on('mouseover',show).on('mouseout',hide);
+  		this.on('mouseover',showTooltipBlock).on('mouseout',hideTooltipBlock);
   		return this;
   };
 })(jQuery);
